9fc68fb63f27d07a379a6300b8534d910c6c3453
苏明月 <sumingyue@xdf.cn>
2018-07-01 00:01:55 +0800

    spring-eureka-sleuth

---
+++ spring-boot-eureka-consumer-sleuth-trace01/src/main/java/com/example/springbooteurekaconsumersleuthtrace01/DcController.java
@@ -0,0 +1,31 @@
+package com.example.springbooteurekaconsumersleuthtrace01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class DcController {
+
+    private final static Logger logger = LoggerFactory.getLogger(DcController.class);
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(name = "/consumer",method = RequestMethod.GET)
+    public String dc(HttpServletRequest request) {
+        logger.info("=================call client hi traceId={},spanId={},parentSpanId={},span-Name={}",
+                request.getHeader("X-B3-TraceId"),
+                request.getHeader("X-B3-SpanId"),
+                request.getHeader("X-B3-ParentSpanId"),
+                request.getHeader("X-Span-Name"));
+        return restTemplate.getForObject("http://eureka-client/hi/11111", String.class);
+    }
+}
